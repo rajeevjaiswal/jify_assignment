36b65dc5cb0361c8c6f0128ac2a29cea6b6461a5
Rajeev Jaiswal <rajeev1.jaiswal@ril.com>
2021-11-14 12:49:49 +0530

    created project structure and added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'src/config/routes/app_routes.dart';
+import 'src/config/themes/app_theme.dart';
+import 'src/core/utils/constants.dart';
+
+Future<void> main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Gallery',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      title: kMaterialAppTitle,
+      onGenerateRoute: AppRoutes.onGenerateRoutes,
+      theme: AppTheme.light,
     );
   }
 }
